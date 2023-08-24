181cdb7b8352b76f4b7424590e3a7e03e071552d
Echo <tzu-mingliu@qq.com>
2023-08-24 17:00:01 +0800

    version：v1.1 perf: 1、优化多级下拉框题执行速度 2、暂时关闭多线程执行方式 feat: 1、新增题型（矩阵填空题） 2、将主程序入口提取到run文件

---
+++ run.py
@@ -0,0 +1,13 @@
+"""         ==Coding: UTF-8==
+**    @Project :        Sojump
+**    @fileName         run.py
+**    @version          v1.1
+**    @author           Echo
+**    @Warehouse        https://gitee.com/liu-long068/
+**    @EditTime         2023/8/24
+"""
+from src.common.executive_sojump import run
+
+if __name__ == '__main__':
+    count = 0  # 初始提交份数
+    run(0, 0)
